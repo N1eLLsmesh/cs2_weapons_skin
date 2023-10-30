679cd0832db4d1dd3c47b43081e7e5ff270f2bd1
Zignixx <cobra1989@gmail.com>
2023-10-30 14:50:49 +0000

    revert

--- Skin.cpp
+++ Skin.cpp
@@ -257,20 +257,16 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 		if(skin_parm == weapon->second.end()) return;
 
 		weaponId = skin_parm->second.m_iItemDefinitionIndex;
-
-		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemIDLow() = -1;
-		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemIDHigh() = 16385;
-
+		
+		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
 		pBasePlayerWeapon->m_nFallbackPaintKit() = skin_parm->second.m_nFallbackPaintKit;
 		pBasePlayerWeapon->m_nFallbackSeed() = skin_parm->second.m_nFallbackSeed;
 		pBasePlayerWeapon->m_flFallbackWear() = skin_parm->second.m_flFallbackWear;
-		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
 
+		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemIDLow() = -1;
+		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemIDHigh() = 16384;
 
-		pBasePlayerWeapon->m_AttributeManager().m_Item().m_iAccountID() = 271098320;
-		pBasePlayerWeapon->m_AttributeManager().m_Item().m_hPrevOwner() = 271098320;
-
-
+		META_CONPRINTF( "DEBUG: %d\n", pBasePlayerWeapon->m_nSubclassID());
 
 		// pBasePlayerWeapon->m_nSubclassID() = skin_parm->second.m_iItemDefinitionIndex;
 		META_CONPRINTF( "steamId: %lld itemId: %d itemId2: %d\n", steamid, weaponId, pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex());
