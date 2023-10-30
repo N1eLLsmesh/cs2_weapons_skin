0bee351ae59a307aee533c4852a4c81a2bdb737c
Zignixx <cobra1989@gmail.com>
2023-10-30 12:15:18 +0000

    ...

--- sdk/CPlayer_ItemServices.h
+++ sdk/CPlayer_ItemServices.h
@@ -4,6 +4,17 @@
 #include "ehandle.h"
 #include "CBaseFlex.h"
 
+template <typename T>
+class CNetworkUtlVectorBase {
+   public:
+    auto begin() const { return m_data; }
+    auto end() const { return m_data + m_size; }
+
+    int m_size;
+    char pad0[0x4];  // no idea
+    T* m_data;
+};
+
 class CEconEntity : public CBaseFlex
 {
 public:
