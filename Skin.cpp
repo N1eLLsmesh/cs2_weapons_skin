739945b2b112d752f0c9d306932a48aa255fd81d
Zignixx <cobra1989@gmail.com>
2023-10-31 10:34:08 +0000

    ops

--- Skin.cpp
+++ Skin.cpp
@@ -334,7 +334,7 @@ CON_COMMAND_F(skin, "modify skin", FCVAR_CLIENT_CAN_EXECUTE) {
 
     CBasePlayerWeapon* pPlayerWeapon = pWeaponServices->m_hActiveWeapon();
 
-	META_CONPRINTF("Current Item: %lld\n", pPlayerWeapon->GetClassname().c_str());
+	META_CONPRINTF("Current Item: %s\n", pPlayerWeapon->GetClassname());
 
     pWeaponServices->RemoveWeapon(pPlayerWeapon);
     FnEntityRemove(g_pGameEntitySystem, pPlayerWeapon, nullptr, -1);
