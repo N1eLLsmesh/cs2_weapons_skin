85661d7c3d7584d70ed02867a4332400ba74da85
Zignixx <cobra1989@gmail.com>
2023-10-31 12:32:05 +0000

    so?

--- sdk/CPlayer_ItemServices.h
+++ sdk/CPlayer_ItemServices.h
@@ -105,13 +105,13 @@ public:
 class CAttributeContainer
 {
 public:
-	SCHEMA_FIELD(CEconItemView, CAttributeContainer, m_Item);
+	PSCHEMA_FIELD(CEconItemView, CAttributeContainer, m_Item);
 };
 
 class CBasePlayerWeapon : public CEconEntity
 {
 public:
-	SCHEMA_FIELD(CAttributeContainer, CEconEntity, m_AttributeManager);
+	PSCHEMA_FIELD(CAttributeContainer, CEconEntity, m_AttributeManager);
 	SCHEMA_FIELD(int32_t, CEconEntity, m_nFallbackPaintKit);
 	SCHEMA_FIELD(int32_t, CEconEntity, m_nFallbackSeed);
 	SCHEMA_FIELD(int32_t, CEconEntity, m_nFallbackStatTrak);
