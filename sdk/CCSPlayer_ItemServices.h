affe5938e28e80f53f6e1b00177ff176acdb57d7
Zignixx <cobra1989@gmail.com>
2023-11-02 12:25:26 +0000

    second test

--- sdk/CCSPlayer_ItemServices.h
+++ sdk/CCSPlayer_ItemServices.h
@@ -22,10 +22,10 @@ private:
 	virtual void unk_12() = 0;
 	virtual void unk_13() = 0;
 	virtual void unk_14() = 0;
-	virtual SC_CBaseEntity* _GiveNamedItem(const char* pchName) = 0;
+	virtual SC_CBaseEntity* _GiveNamedItem(const char* pchName, int iSubType = 0, bool removeIfNotCarried = true ) = 0;
 public:
-	virtual bool GiveNamedItemBool(const char* pchName) = 0;
-	virtual SC_CBaseEntity* GiveNamedItem(const char* pchName) = 0;
+	virtual bool GiveNamedItemBool(const char* pchName, int iSubType = 0, bool removeIfNotCarried = true ) = 0;
+	virtual SC_CBaseEntity* GiveNamedItem(const char* pchName, int iSubType = 0, bool removeIfNotCarried = true ) = 0;
 
 	SCHEMA_FIELD(bool, CCSPlayer_ItemServices, m_bHasDefuser);
 	SCHEMA_FIELD(bool, CCSPlayer_ItemServices, m_bHasHelmet);
