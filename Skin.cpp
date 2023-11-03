df98bf46bf1aef0df0286af29018b00550ca8c63
Zignixx <cobra1989@gmail.com>
2023-11-03 13:12:05 +0000

    <.<

--- Skin.cpp
+++ Skin.cpp
@@ -312,10 +312,6 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 	});
 }
 
-CON_COMMAND_CHAT(ws, "modify skin") {
-	META_CONPRINTF( "CON_COMMAND_CHAT\n");
-}
-
 CON_COMMAND_F(skin, "modify skin", FCVAR_CLIENT_CAN_EXECUTE) {
     if (context.GetPlayerSlot() == -1) {
 		return;
--- Skin.h
+++ Skin.h
@@ -51,22 +51,3 @@ class CEntityListener : public IEntityListener
 };
 
 #endif //_INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
-
-
-#define CON_COMMAND_CHAT_FLAGS(name, description, flags)																								\
-	void name##_callback(const CCommand &args, CCSPlayerController *player);																			\
-	static CChatCommand name##_chat_command(#name, name##_callback, flags);																				\
-	static void name##_con_callback(const CCommandContext &context, const CCommand &args)																\
-	{																																					\
-		CCSPlayerController *pController = nullptr;																										\
-		if (context.GetPlayerSlot().Get() != -1)																										\
-			pController = (CCSPlayerController *)g_pEntitySystem->GetBaseEntity((CEntityIndex)(context.GetPlayerSlot().Get() + 1));						\
-																																						\
-		name##_chat_command(args, pController);																											\
-	}																																					\
-	static ConCommandRefAbstract name##_ref;																											\
-	static ConCommand name##_command(&name##_ref, COMMAND_PREFIX #name, name##_con_callback,															\
-									description, FCVAR_CLIENT_CAN_EXECUTE | FCVAR_LINKED_CONCOMMAND);													\
-	void name##_callback(const CCommand &args, CCSPlayerController *player)
-
-#define CON_COMMAND_CHAT(name, description) CON_COMMAND_CHAT_FLAGS(name, description, ADMFLAG_NONE)
