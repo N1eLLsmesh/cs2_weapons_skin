83e130bbce4019a47f8a51c88c6ec1d0104d6598
Zignixx <cobra1989@gmail.com>
2023-11-04 17:45:34 +0000

    or maybe this?

--- Skin.cpp
+++ Skin.cpp
@@ -389,7 +389,8 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 		
 		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
 		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemIDLow() = -1;
-		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemIDHigh() = -1;
+		static int IDHigh = 16384;
+		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemIDHigh() = IDHigh++;
 		pCEconEntityWeapon->m_nFallbackPaintKit() = skin_parm->second.m_nFallbackPaintKit;
 		pCEconEntityWeapon->m_nFallbackSeed() = skin_parm->second.m_nFallbackSeed;
 		pCEconEntityWeapon->m_flFallbackWear() = skin_parm->second.m_flFallbackWear;
