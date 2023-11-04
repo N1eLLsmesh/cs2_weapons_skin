c134769dabec27761169db4ec8a54d2fdd46ea9a
Zignixx <cobra1989@gmail.com>
2023-11-04 16:44:56 +0000

    looks good for the steamid now

--- Skin.cpp
+++ Skin.cpp
@@ -353,17 +353,11 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 	if(!pBasePlayerWeapon) return;
 	g_Skin.NextFrame([pBasePlayerWeapon = pBasePlayerWeapon, pCEconEntityWeapon = pCEconEntityWeapon]()
 	{
-		int32_t steamid = pCEconEntityWeapon->m_OriginalOwnerXuidLow();
-		int32_t steamid1 = pCEconEntityWeapon->m_OriginalOwnerXuidHigh();
+		int32_t steamid = pCEconEntityWeapon->m_OriginalOwnerXuidLow() | (static_cast<int64_t>(pCEconEntityWeapon->m_OriginalOwnerXuidHigh()) << 32);
 		int64_t weaponId = pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex();
 
 		// print the steamid and weaponId
 		META_CONPRINTF( "steamId: %lld itemId: %d\n", steamid, weaponId);
-		META_CONPRINTF( "steamId1: %lld itemId: %d\n", steamid1, weaponId);
-
-		// combine steamid and steamid1 (low and high) into a single 64 bit steamid
-		int64_t steamid2 = steamid | (static_cast<int64_t>(steamid1) << 32);
-		META_CONPRINTF( "steamId2: %lld itemId: %d\n", steamid2, weaponId);
 
 		if(!steamid) {
 			return;
