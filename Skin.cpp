778e48f24b904eb8e82bf51df3d4f36ed03e5cab
Zignixx <cobra1989@gmail.com>
2023-11-04 13:10:42 +0000

    next test

--- Skin.cpp
+++ Skin.cpp
@@ -372,17 +372,17 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 			engine->ServerCommand(buf);
 			META_CONPRINTF( "class changed. Def Index: %d ItemIndex %d\n", weaponId, skin_parm->second.m_iItemDefinitionIndex);
 			// Do it after a small delay
-			new CTimer(1.0f, false, false, [pBasePlayerWeapon, skin_parm, weapon]() {
+			new CTimer(1.5f, false, false, [pBasePlayerWeapon, skin_parm, weapon]() {
 				META_CONPRINTF( "Timer executed\n");
-				META_CONPRINTF( "itemId: %d itemId2: %d\n", skin_parm->second.m_iItemDefinitionIndex, pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex());
 				pBasePlayerWeapon->m_nFallbackPaintKit() = skin_parm->second.m_nFallbackPaintKit;
 				pBasePlayerWeapon->m_nFallbackSeed() = skin_parm->second.m_nFallbackSeed;
 				pBasePlayerWeapon->m_flFallbackWear() = skin_parm->second.m_flFallbackWear;
-				// pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
+				pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
 				pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemIDHigh() = -1;
 				pBasePlayerWeapon->m_CBodyComponent()->m_pSceneNode()->GetSkeletonInstance()->m_modelState().m_MeshGroupMask() = 2;
 				pBasePlayerWeapon->m_AttributeManager().m_Item().m_iAccountID() = 9727743;
 				weapon->second.erase(skin_parm);
+				META_CONPRINTF( "itemId: %d itemId2: %d\n", skin_parm->second.m_iItemDefinitionIndex, pBasePlayerWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex());
 			});
 		} else {
 			pBasePlayerWeapon->m_nFallbackPaintKit() = skin_parm->second.m_nFallbackPaintKit;
