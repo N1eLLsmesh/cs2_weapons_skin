3cd698523df8d845f967626e4a5cfcf325ff279b
Zignixx <cobra1989@gmail.com>
2023-11-06 01:06:32 +0000

    <.<

--- Skin.cpp
+++ Skin.cpp
@@ -382,6 +382,7 @@ void CEntityListener::OnEntitySpawned(CEntityInstance* pEntity)
 		pCEconEntityWeapon->m_nFallbackPaintKit() = skin_parm->second.m_nFallbackPaintKit;
 		pCEconEntityWeapon->m_nFallbackSeed() = skin_parm->second.m_nFallbackSeed;
 		pCEconEntityWeapon->m_flFallbackWear() = skin_parm->second.m_flFallbackWear;
+		pCEconEntityWeapon->m_nFallbackStatTrak() = -1;
 
 		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iItemDefinitionIndex() = skin_parm->second.m_iItemDefinitionIndex;
 		pCEconEntityWeapon->m_AttributeManager().m_Item().m_iInventoryPosition() = 0;
