1080ee4591664ba762ec33b3d56157763a4f79f0
Zignixx <cobra1989@gmail.com>
2023-11-08 13:31:56 +0000

    ok

--- sdk/schemasystem.h
+++ sdk/schemasystem.h
@@ -22,6 +22,21 @@
 #define strtok_s strtok_r
 #endif
 
+constexpr uint32_t val_32_const = 0x811c9dc5;
+constexpr uint32_t prime_32_const = 0x1000193;
+constexpr uint64_t val_64_const = 0xcbf29ce484222325;
+constexpr uint64_t prime_64_const = 0x100000001b3;
+
+inline constexpr uint32_t hash_32_fnv1a_const(const char *const str, const uint32_t value = val_32_const) noexcept
+{
+	return (str[0] == '\0') ? value : hash_32_fnv1a_const(&str[1], (value ^ uint32_t(str[0])) * prime_32_const);
+}
+
+inline constexpr uint64_t hash_64_fnv1a_const(const char *const str, const uint64_t value = val_64_const) noexcept
+{
+	return (str[0] == '\0') ? value : hash_64_fnv1a_const(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
+}
+
 #define CALL_VIRTUAL(retType, idx, ...) \
     vmt::CallVirtual<retType>(idx, __VA_ARGS__)
 namespace vmt {
