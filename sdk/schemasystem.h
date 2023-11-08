f94d1957f6d6d2bf83a45bd8cbfee4c47b78fe35
Zignixx <cobra1989@gmail.com>
2023-11-08 13:36:10 +0000

    testing new schemasystem from cs2fixes

--- sdk/schemasystem.h
+++ sdk/schemasystem.h
@@ -1,26 +1,51 @@
-//====== Copyright �, Valve Corporation, All rights reserved. =======
-//
-// Purpose: Additional shared object cache functionality for the GC
-//
-//=============================================================================
+/**
+ * =============================================================================
+ * CS2Fixes
+ * Copyright (C) 2023 Source2ZE
+ * =============================================================================
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3.0, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef SCHEMASYSTEM_H
-#define SCHEMASYSTEM_H
-#ifdef _WIN32
 #pragma once
+
+#include "stdint.h"
+
+#ifdef _WIN32
+#pragma warning(push)
+#pragma warning(disable : 4005)
 #endif
 
-#include <cstdint>
 #include <type_traits>
 
-#if defined(_WIN32)
-#define FASTCALL __fastcall
-#define THISCALL __thiscall
-#else
-#define FASTCALL __attribute__((fastcall))
-#define THISCALL
-#define strtok_s strtok_r
+#ifdef _WIN32
+#pragma warning(pop)
 #endif
+
+#include "../addresses.h"
+#include "tier0/dbg.h"
+#include "const.h"
+#include "virtual.h"
+#include "stdint.h"
+#undef schema
+
+struct SchemaKey {
+	int16_t offset;
+	bool networked;
+};
+
+class Z_CBaseEntity;
+void SetStateChanged(Z_CBaseEntity* pEntity, int offset);
 
 constexpr uint32_t val_32_const = 0x811c9dc5;
 constexpr uint32_t prime_32_const = 0x1000193;
@@ -37,72 +62,106 @@ inline constexpr uint64_t hash_64_fnv1a_const(const char *const str, const uint6
 	return (str[0] == '\0') ? value : hash_64_fnv1a_const(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
 }
 
-#define CALL_VIRTUAL(retType, idx, ...) \
-    vmt::CallVirtual<retType>(idx, __VA_ARGS__)
-namespace vmt {
-	template <typename T = void*>
-	inline T GetVMethod(uint32_t uIndex, void* pClass) {
-		void** pVTable = *static_cast<void***>(pClass);
-		return reinterpret_cast<T>(pVTable[uIndex]);
-	}
+#define SCHEMA_FIELD_OFFSET(type, varName, extra_offset)																	\
+	class varName##_prop																									\
+	{																														\
+	public:																													\
+		std::add_lvalue_reference_t<type> Get()																				\
+		{																													\
+			static constexpr auto datatable_hash = hash_32_fnv1a_const(ThisClassName);										\
+			static constexpr auto prop_hash = hash_32_fnv1a_const(#varName);												\
+																															\
+			static const auto m_key =																						\
+				schema::GetOffset(ThisClassName, datatable_hash, #varName, prop_hash);										\
+																															\
+			static const size_t offset = offsetof(ThisClass, varName);														\
+			ThisClass *pThisClass = (ThisClass *)((byte *)this - offset);													\
+																															\
+			return *reinterpret_cast<std::add_pointer_t<type>>(																\
+				(uintptr_t)(pThisClass) + m_key.offset + extra_offset);														\
+		}																													\
+		void Set(type val)																									\
+		{																													\
+			static constexpr auto datatable_hash = hash_32_fnv1a_const(ThisClassName);										\
+			static constexpr auto prop_hash = hash_32_fnv1a_const(#varName);												\
+																															\
+			static const auto m_key =																						\
+				schema::GetOffset(ThisClassName, datatable_hash, #varName, prop_hash);										\
+																															\
+			static const auto m_chain =																						\
+				schema::FindChainOffset(ThisClassName);																		\
+																															\
+			static const size_t offset = offsetof(ThisClass, varName);														\
+			ThisClass *pThisClass = (ThisClass *)((byte *)this - offset);													\
+																															\
+			if (m_chain != 0 && m_key.networked)																			\
+			{																												\
+				DevMsg("Found chain offset %d for %s::%s\n", m_chain, ThisClassName, #varName);								\
+				addresses::NetworkStateChanged((uintptr_t)(pThisClass) + m_chain, m_key.offset + extra_offset, 0xFFFFFFFF);	\
+			}																												\
+			else if(m_key.networked)																						\
+			{																												\
+				/* WIP: Works fine for most props, but inlined classes in the middle of a class will
+					need to have their this pointer corrected by the offset .*/												\
+				DevMsg("Attempting to call SetStateChanged on on %s::%s\n", ThisClassName, #varName);						\
+				if (!IsStruct)																								\
+					SetStateChanged((Z_CBaseEntity*)pThisClass, m_key.offset + extra_offset);								\
+				else if (IsPlatformPosix()) /* This is currently broken on windows */										\
+					CALL_VIRTUAL(void, 1, pThisClass, m_key.offset + extra_offset, 0xFFFFFFFF, 0xFFFF);						\
+			}																												\
+			*reinterpret_cast<std::add_pointer_t<type>>((uintptr_t)(pThisClass) + m_key.offset + extra_offset) = val;		\
+		}																													\
+		operator std::add_lvalue_reference_t<type>() { return Get(); }														\
+		std::add_lvalue_reference_t<type> operator ()() { return Get(); }													\
+		std::add_lvalue_reference_t<type> operator->() { return Get(); }													\
+		void operator()(type val) { Set(val); }																				\
+		void operator=(type val) { Set(val); }																				\
+	} varName;
 
-	template <typename T, typename... Args>
-	inline T CallVirtual(uint32_t uIndex, void* pClass, Args... args) {
-		auto pFunc = GetVMethod<T(THISCALL*)(void*, Args...)>(uIndex, pClass);
-		return pFunc(pClass, args...);
-	}
-}  // namespace vmt
+#define SCHEMA_FIELD_POINTER_OFFSET(type, varName, extra_offset)															\
+	class varName##_prop																									\
+	{																														\
+	public:																													\
+		type *Get()																											\
+		{																													\
+			static constexpr auto datatable_hash = hash_32_fnv1a_const(ThisClassName);										\
+			static constexpr auto prop_hash = hash_32_fnv1a_const(#varName);												\
+																															\
+			static const auto m_key =																						\
+				schema::GetOffset(ThisClassName, datatable_hash, #varName, prop_hash);										\
+																															\
+			static const size_t offset = offsetof(ThisClass, varName);														\
+			ThisClass *pThisClass = (ThisClass *)((byte *)this - offset);													\
+																															\
+			return reinterpret_cast<std::add_pointer_t<type>>(																\
+				(uintptr_t)(pThisClass) + m_key.offset + extra_offset);														\
+		}																													\
+		operator type*() { return Get(); }																					\
+		type* operator ()() { return Get(); }																				\
+		type* operator->() { return Get(); }																				\
+	} varName;
 
-struct SchemaClassFieldData_t
+// Use this when you want the member's value itself
+#define SCHEMA_FIELD(type, varName) \
+	SCHEMA_FIELD_OFFSET(type, varName, 0)
+
+// Use this when you want a pointer to a member
+#define SCHEMA_FIELD_POINTER(type, varName) \
+	SCHEMA_FIELD_POINTER_OFFSET(type, varName, 0)
+
+namespace schema
 {
-    const char* m_pszName; // 0x0000
-    void* m_pSchemaType; // 0x0008
-    int32_t m_iOffset; // 0x0010
-    int32_t m_iMetaDataSize; // 0x0014
-    void* m_pMetaData; // 0x0018
-};
+	int16_t FindChainOffset(const char *className);
+	SchemaKey GetOffset(const char *className, uint32_t classKey, const char *memberName, uint32_t memberKey);
+}
 
-struct SchemaClassInfoData_t
-{
-    char pad_0x0000[0x8]; // 0x0000
+#define DECLARE_SCHEMA_CLASS_BASE(className, isStruct)			\
+	typedef className ThisClass;								\
+	static constexpr const char *ThisClassName = #className;	\
+	static constexpr bool IsStruct = isStruct;				  
 
-    const char* m_pszName; // 0x0008
-    const char* m_pszModule; // 0x0010
+#define DECLARE_SCHEMA_CLASS(className) DECLARE_SCHEMA_CLASS_BASE(className, false)
 
-    int m_iSize; // 0x0018
-    int16_t m_iFieldsCount; // 0x001C
-
-    int16_t m_iStaticSize; // 0x001E
-    int16_t m_iMetadataSize; // 0x0020
-    int16_t m_iUnk1; // 0x0022
-    int16_t m_iUnk2; // 0x0024
-    int16_t m_iUnk3; // 0x0026
-
-    SchemaClassFieldData_t* m_pFieldsData; // 0x0028
-};
-
-class CSchemaSystemTypeScope
-{
-public:
-    void FindDeclaredClass(SchemaClassInfoData_t*& pClassInfo, const char* pszClassName);
-};
-
-class CSchemaSystem
-{
-public:
-    CSchemaSystemTypeScope* FindTypeScopeForModule(const char* szpModuleName);
-    CSchemaSystemTypeScope* GetServerTypeScope();
-    int32_t GetServerOffset(const char* pszClassName, const char* pszPropName);
-};
-
-extern CSchemaSystem* g_pCSchemaSystem;
-
-#define SCHEMA_FIELD(type, className, propName)                                                        \
-    std::add_lvalue_reference_t<type> propName()                                                       \
-    {                                                                                                  \
-        static const int32_t offset = g_pCSchemaSystem->GetServerOffset(#className, #propName);         \
-        return *reinterpret_cast<std::add_pointer_t<type>>(reinterpret_cast<intptr_t>(this) + offset); \
-    }
-
-
-#endif //SCHEMASYSTEM_H
+// Use this for classes that can be wholly included within other classes (like CCollisionProperty within CBaseModelEntity)
+#define DECLARE_SCHEMA_CLASS_INLINE(className) \
+	DECLARE_SCHEMA_CLASS_BASE(className, true)
