a9cc30f410adeff9256dc5a00f804c9e044a03d0
Zignixx <cobra1989@gmail.com>
2023-11-08 13:39:09 +0000

    better not do that

--- sdk/schemasystem.cpp
+++ sdk/schemasystem.cpp
@@ -1,142 +1,44 @@
-/**
- * =============================================================================
- * CS2Fixes
- * Copyright (C) 2023 Source2ZE
- * =============================================================================
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, version 3.0, as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+#include "schemasystem.h"
+#include "utils.hpp"
+#include <cstring>
 
-#include "schema.h"
-
-#include "../common.h"
-#include "interfaces/cs2_interfaces.h"
-#include "tier1/utlmap.h"
-#include "tier0/memdbgon.h"
-#include "plat.h"
-#include "entity/cbaseentity.h"
-
-extern CGlobalVars *gpGlobals;
-
-using SchemaKeyValueMap_t = CUtlMap<uint32_t, SchemaKey>;
-using SchemaTableMap_t = CUtlMap<uint32_t, SchemaKeyValueMap_t*>;
-
-
-static bool IsFieldNetworked(SchemaClassFieldData_t& field)
+void CSchemaSystemTypeScope::FindDeclaredClass(SchemaClassInfoData_t*& pClassInfo, const char* pszClassName)
 {
-    for (int i = 0; i < field.m_metadata_size; i++)
-    {
-        static auto networkEnabled = hash_32_fnv1a_const("MNetworkEnable");
-        if (networkEnabled == hash_32_fnv1a_const(field.m_metadata[i].m_name))
-            return true;
-    }
-
-    return false;
-}   
-
-static bool InitSchemaFieldsForClass(SchemaTableMap_t *tableMap, const char* className, uint32_t classKey)
-{
-    CSchemaSystemTypeScope* pType = interfaces::pSchemaSystem->FindTypeScopeForModule(MODULE_PREFIX "server" MODULE_EXT);
-
-    if (!pType)
-        return false;
-
-    SchemaClassInfoData_t *pClassInfo = pType->FindDeclaredClass(className);
-
-    if (!pClassInfo)
-    {
-        SchemaKeyValueMap_t *map = new SchemaKeyValueMap_t(0, 0, DefLessFunc(uint32_t));
-        tableMap->Insert(classKey, map);
-
-        Warning("InitSchemaFieldsForClass(): '%s' was not found!\n", className);
-        return false;
-    }
-
-    short fieldsSize = pClassInfo->GetFieldsSize();
-    SchemaClassFieldData_t* pFields = pClassInfo->GetFields();
-
-    SchemaKeyValueMap_t *keyValueMap = new SchemaKeyValueMap_t(0, 0, DefLessFunc(uint32_t));
-    keyValueMap->EnsureCapacity(fieldsSize);
-    tableMap->Insert(classKey, keyValueMap);
-
-    for (int i = 0; i < fieldsSize; ++i)
-    {
-        SchemaClassFieldData_t& field = pFields[i];
-
-#ifdef _DEBUG
-        Message("%s::%s found at -> 0x%X - %llx\n", className, field.m_name, field.m_offset, &field);
+#if defined _WIN32 && _M_X64
+    CallVFunc<void, 2, SchemaClassInfoData_t*&, const char*>(this, pClassInfo, pszClassName);
+#else
+    pClassInfo = CallVFunc<SchemaClassInfoData_t*, 2, const char*>(this, pszClassName);
 #endif
-
-        keyValueMap->Insert(hash_32_fnv1a_const(field.m_name), { field.m_offset, IsFieldNetworked(field) });
-    }
-
-    return true;
 }
 
-int16_t schema::FindChainOffset(const char* className)
+CSchemaSystemTypeScope* CSchemaSystem::FindTypeScopeForModule(const char* szpModuleName)
 {
-    CSchemaSystemTypeScope* pType = interfaces::pSchemaSystem->FindTypeScopeForModule(MODULE_PREFIX "server" MODULE_EXT);
+    return CallVFunc<CSchemaSystemTypeScope*, 13, const char*, void*>(this, szpModuleName, nullptr);
+}
 
-    if (!pType)
-        return false;
+CSchemaSystemTypeScope* CSchemaSystem::GetServerTypeScope()
+{
+    static CSchemaSystemTypeScope* pServerTypeScope = FindTypeScopeForModule(WIN_LINUX("server.dll", "libserver.so"));
 
-    SchemaClassInfoData_t* pClassInfo = pType->FindDeclaredClass(className);
+    return pServerTypeScope;
+}
 
-    do
+int32_t CSchemaSystem::GetServerOffset(const char* pszClassName, const char* pszPropName)
+{
+    SchemaClassInfoData_t* pClassInfo = nullptr;
+    GetServerTypeScope()->FindDeclaredClass(pClassInfo, pszClassName);
+    if (pClassInfo)
     {
-        SchemaClassFieldData_t* pFields = pClassInfo->GetFields();
-        short fieldsSize = pClassInfo->GetFieldsSize();
-        for (int i = 0; i < fieldsSize; ++i)
+        for (int i = 0; i < pClassInfo->m_iFieldsCount; i++)
         {
-            SchemaClassFieldData_t& field = pFields[i];
+            auto& pFieldData = pClassInfo->m_pFieldsData[i];
 
-            if (V_strcmp(field.m_name, "__m_pChainEntity") == 0)
+            if (std::strcmp(pFieldData.m_pszName, pszPropName) == 0)
             {
-                return field.m_offset;
+                return pFieldData.m_iOffset;
             }
         }
-    } while ((pClassInfo = pClassInfo->GetParent()) != nullptr);
-
-    return 0;
-}
-
-SchemaKey schema::GetOffset(const char* className, uint32_t classKey, const char* memberName, uint32_t memberKey)
-{
-	static SchemaTableMap_t schemaTableMap(0, 0, DefLessFunc(uint32_t));
-	int16_t tableMapIndex = schemaTableMap.Find(classKey);
-    if (!schemaTableMap.IsValidIndex(tableMapIndex))
-    {
-        if (InitSchemaFieldsForClass(&schemaTableMap, className, classKey))
-            return GetOffset(className, classKey, memberName, memberKey);
-
-        return { 0, 0 };
     }
 
-    SchemaKeyValueMap_t *tableMap = schemaTableMap[tableMapIndex];
-	int16_t memberIndex = tableMap->Find(memberKey);
-    if (!tableMap->IsValidIndex(memberIndex))
-    {
-        Warning("schema::GetOffset(): '%s' was not found in '%s'!\n", memberName, className);
-        return { 0, 0 };
-    }
-
-    return tableMap->Element(memberIndex);
+    return -1;
 }
-
-void SetStateChanged(Z_CBaseEntity* pEntity, int offset)
-{
-    addresses::StateChanged(pEntity->m_NetworkTransmitComponent(), pEntity, offset, -1, -1);
-	
-    pEntity->m_lastNetworkChange = gpGlobals->curtime;
-    pEntity->m_isSteadyState().ClearAll();
-};
